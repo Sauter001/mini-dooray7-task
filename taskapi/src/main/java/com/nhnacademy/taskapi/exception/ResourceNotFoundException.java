64d6aa8054b385cda2e876aeb74d2b00aae99d4c
김도현 <sjj2305@naver.com>
2024-10-12 21:34:17 +0900

    에러메세지 수정

--- taskapi/src/main/java/com/nhnacademy/taskapi/exception/ResourceNotFoundException.java
+++ taskapi/src/main/java/com/nhnacademy/taskapi/exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.nhnacademy.taskapi.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String string) {
-        super(string);
+    public ResourceNotFoundException(String resourceName) {
+        super("Resource '" + resourceName + "' not found");
     }
 }
