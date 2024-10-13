3c703033bd17ef86bee5164e492f6cf78f0aa982
Sauter001 <lsj457@naver.com>
2024-10-13 22:00:50 +0900

    project-component-test.sql 수정

--- taskapi/src/test/resources/com/nhnacademy/taskapi/repository/project-component-test.sql
+++ taskapi/src/test/resources/com/nhnacademy/taskapi/repository/project-component-test.sql
@@ -1,10 +1,12 @@
-insert into project(name, project_status)
-values ('dooray-active1', 'ACTIVE');
+insert into account values (1);
+
+insert into project(account_id, project_name, project_status)
+values (1,'dooray-active1', 'ACTIVE');
 
 insert into milestone(project_id, progress)
 values (1, 'UI 설계'),
        (1, 'DB 설계'),
-       (1, '테스트'),
+       (1, '테스트');
 
 insert into tag (project_id, tag_name)
 values (1, 'DB'), (1, 'UI');
