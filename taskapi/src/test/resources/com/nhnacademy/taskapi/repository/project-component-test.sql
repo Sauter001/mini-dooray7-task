0950fd0d2f232fe4a471b0f1f33f26f7b7e65599
Sauter001 <lsj457@naver.com>
2024-10-13 22:03:29 +0900

    project-component-test.sql id column 추가

--- taskapi/src/test/resources/com/nhnacademy/taskapi/repository/project-component-test.sql
+++ taskapi/src/test/resources/com/nhnacademy/taskapi/repository/project-component-test.sql
@@ -1,13 +1,15 @@
-insert into account values (1);
+insert into account
+values (1);
 
-insert into project(account_id, project_name, project_status)
-values (1,'dooray-active1', 'ACTIVE');
+insert into project(project_id, account_id, project_name, project_status)
+values (1, 1, 'dooray-active1', 'ACTIVE');
 
-insert into milestone(project_id, progress)
-values (1, 'UI 설계'),
-       (1, 'DB 설계'),
-       (1, '테스트');
+insert into milestone(mile_stone_id, project_id, progress)
+values (1, 1, 'UI 설계'),
+       (2, 1, 'DB 설계'),
+       (3, 1, '테스트');
 
 insert into tag (project_id, tag_name)
-values (1, 'DB'), (1, 'UI');
+values (1, 'DB'),
+       (1, 'UI');
        
