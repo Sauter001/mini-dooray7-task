36cfe0752fcac6569f4c712819020a0a0b6848a2
Sauter001 <lsj457@naver.com>
2024-10-13 20:59:57 +0900

    fix: 업무 태그 관련 entity relation 오류 해결

--- taskapi/src/main/java/com/nhnacademy/taskapi/controller/MileStoneController.java
+++ taskapi/src/main/java/com/nhnacademy/taskapi/controller/MileStoneController.java
@@ -20,34 +20,34 @@ public class MileStoneController {
     private final MilestoneService milestoneService;
 
     @GetMapping("/{projectId}/milestones")
-    public ResponseEntity<DefaultDto> getMilestones(@PathVariable("projectId") Long projectId) {
+    public ResponseEntity<DefaultDto<List<MilestoneView>>> getMilestones(@PathVariable("projectId") Long projectId) {
         List<MilestoneView> milestones = milestoneService.getMilestones(projectId);
-        DefaultDto dto = new DefaultDto(200, milestones);
+        DefaultDto<List<MilestoneView>> dto = new DefaultDto<>(200, milestones);
         return ResponseEntity.ok(dto);
     }
 
     @PostMapping("/{projectId}/milestones")
-    public ResponseEntity<DefaultDto> postMilestone(@PathVariable("projectId") Long projectId,
+    public ResponseEntity<DefaultDto<Object>> postMilestone(@PathVariable("projectId") Long projectId,
                                                     @RequestBody MilestonePostDto postDto) {
         milestoneService.post(projectId, postDto);
-        DefaultDto dto = new DefaultDto(201, null);
+        DefaultDto<Object> dto = new DefaultDto<>(201, null);
 
         return ResponseEntity.status(201).body(dto);
     }
 
     @PutMapping("/{projectId}/milestones/{milestoneId}")
-    public ResponseEntity<DefaultDto> updateMilestone(@PathVariable Long projectId, @PathVariable Long milestoneId,
+    public ResponseEntity<DefaultDto<MilestoneView>> updateMilestone(@PathVariable Long projectId, @PathVariable Long milestoneId,
                                                       @RequestBody MilestonePutDto putDto) {
         MilestoneView milestoneView = milestoneService.update(projectId, milestoneId, putDto);
-        return ResponseEntity.ok(new DefaultDto(200, milestoneView));
+        return ResponseEntity.ok(new DefaultDto<>(200, milestoneView));
     }
 
     @DeleteMapping("/{projectId}/milestones/{milestoneId}")
-    public ResponseEntity<DefaultDto> deleteMilestone(@PathVariable Long projectId, @PathVariable Long milestoneId) {
+    public ResponseEntity<DefaultDto<Object>> deleteMilestone(@PathVariable Long projectId, @PathVariable Long milestoneId) {
         milestoneService.delete(projectId, milestoneId);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(
-                new DefaultDto(HttpServletResponse
+                new DefaultDto<>(HttpServletResponse
                                        .SC_NO_CONTENT, null));
     }
 }
