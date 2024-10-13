95f5cb491959ad73a8d170e40761105de7e43afc
jaaaayk <janghyun30@gmail.com>
2024-10-13 16:51:59 +0900

    Fix : Account, Project, ProjectService 권환 및 연관관계 수정

--- taskapi/src/main/java/com/nhnacademy/taskapi/entity/Account.java
+++ taskapi/src/main/java/com/nhnacademy/taskapi/entity/Account.java
@@ -4,7 +4,10 @@ package com.nhnacademy.taskapi.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,4 +16,12 @@ import lombok.*;
 public class Account {
     @Id
     private Long accountId;
+
+    @OneToMany(mappedBy = "manager")
+    private List<Project> projects;
+
+
+    public Account(Long accountId) {
+        this.accountId = accountId;
+    }
 }
