497c64124fca3c69ec44af716bf091a2f5c9a4a6
Sauter001 <lsj457@naver.com>
2024-10-13 22:12:03 +0900

    test sql 수정

--- taskapi/src/test/java/com/nhnacademy/taskapi/repository/MilestoneRepositoryTest.java
+++ taskapi/src/test/java/com/nhnacademy/taskapi/repository/MilestoneRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -46,5 +48,14 @@ class MilestoneRepositoryTest {
     void findViewByMilestoneId_notFound() {
         MilestoneView milestone = milestoneRepository.findByMileStoneId(1024L).orElse(null);
         assertNull(milestone);
+    }
+
+    @Test
+    @Sql("project-component-test.sql")
+    void findAllByProjectProjectId() {
+        List<MilestoneView> milestoneViews = milestoneRepository.findAllByProjectProjectId(1L);
+        List<String> milestoneNames = List.of("UI 설계", "DB 설계", "테스트");
+
+        assertTrue(milestoneViews.stream().allMatch(m -> milestoneNames.contains(m.getProgress())));
     }
 }
--- taskapi/src/test/resources/com/nhnacademy/taskapi/repository/projects-test.sql
+++ taskapi/src/test/resources/com/nhnacademy/taskapi/repository/projects-test.sql
@@ -1,12 +1,12 @@
-insert into project(name, project_status)
-values ('dooray-active1', 'ACTIVE'),
-       ('dooray-active2', 'ACTIVE'),
-       ('dooray-active3', 'ACTIVE'),
-       ('completed-project1', 'COMPLETE'),
-       ('completed-project2', 'COMPLETE'),
-       ('completed-project3', 'COMPLETE'),
-       ('inactive-project1', 'INACTIVE'),
-       ('inactive-project2', 'INACTIVE'),
-       ('inactive-project3', 'INACTIVE'),
+insert into project(project_id, project_name, project_status)
+values (1, 'dooray-active1', 'ACTIVE'),
+       (2, 'dooray-active2', 'ACTIVE'),
+       (3, 'dooray-active3', 'ACTIVE'),
+       (4, 'completed-project1', 'COMPLETE'),
+       (5, 'completed-project2', 'COMPLETE'),
+       (6, 'completed-project3', 'COMPLETE'),
+       (7, 'inactive-project1', 'INACTIVE'),
+       (8, 'inactive-project2', 'INACTIVE'),
+       (9, 'inactive-project3', 'INACTIVE'),
 
 
